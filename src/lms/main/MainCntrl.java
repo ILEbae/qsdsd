70739f98a79394496a08bf00ce862e704d811d19
seungjun Bae <wnstmdqo94@naver.com>
2023-06-03 23:00:40 +0900

    MainCntrl 오류.. 해결방법 아직모름

--- src/lms/main/MainCntrl.java
+++ src/lms/main/MainCntrl.java
@@ -4,8 +4,8 @@ import lab.LabManager;
 import member.MemberManager;
 
 public class MainCntrl{
-    MemberManager ma = new MemberManager(50);  ma.ini();
-    LabManager lm = new LabManager(50);  lm.ini();
+    MemberManager ma = new MemberManager(50);  ma.ini(); // 여기부분 ini 매서드를 실행시켜줄수없음... 왜그럴까?
+    LabManager lm = new LabManager(50);  lm.ini(); // 여기부분도 ini 매서드 실행불가..
 
 
 
