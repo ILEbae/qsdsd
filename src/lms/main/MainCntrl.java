ae63eda67191d614ce8b91037d11fb1af5e40f1d
seungjun Bae <wnstmdqo94@naver.com>
2023-06-03 23:06:22 +0900

    해결완료

--- src/lms/main/MainCntrl.java
+++ src/lms/main/MainCntrl.java
@@ -4,8 +4,10 @@ import lab.LabManager;
 import member.MemberManager;
 
 public class MainCntrl{
-    MemberManager ma = new MemberManager(50);  ma.ini(); // 여기부분 ini 매서드를 실행시켜줄수없음... 왜그럴까?
-    LabManager lm = new LabManager(50);  lm.ini(); // 여기부분도 ini 매서드 실행불가..
+    public static void main(String[] args) {
+        MemberManager ma = new MemberManager(50);  ma.ini();
+        LabManager lm = new LabManager(50);  lm.ini();
+    }
 
 
 
